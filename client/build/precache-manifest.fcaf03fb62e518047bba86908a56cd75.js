fdbce5739a97c57c2a6caa617c7a68405c0268ce
sonnh <sonnh@toprate.io>
2020-02-27 14:27:32 +0700

    addtion production

---
+++ client/build/precache-manifest.fcaf03fb62e518047bba86908a56cd75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b61da3581f9795bd6ab4cd982724f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d6117502a4fcc622f59",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "37afe116a0268f984794",
+    "url": "/static/css/main.03c1cfc8.chunk.css"
+  },
+  {
+    "revision": "0d6117502a4fcc622f59",
+    "url": "/static/js/2.db74f86f.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.db74f86f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37afe116a0268f984794",
+    "url": "/static/js/main.0fe4d784.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "c1a578e6ab670cf0fa91f859f48fa380",
+    "url": "/static/media/logoo.c1a578e6.svg"
+  }
+]);
